da33950ef2c757207f91a560a6da7094abffa747
Markéta Galdová <marketa@galdova.cz>
2024-12-07 10:09:04 +0100

    07 part 1

---
+++ 07/part1.kts
@@ -0,0 +1,21 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+
+val input = Path("07/input.txt").readLines()
+
+fun check(operands: List<Long>, result: Long): Boolean {
+    operands.singleOrNull()?.let { return it == result }
+    if (check(listOf(operands[0] + operands[1]) + operands.subList(2, operands.size), result)) return true
+    if (check(listOf(operands[0] * operands[1]) + operands.subList(2, operands.size), result)) return true
+    return false
+}
+
+var total = 0L
+for (line in input) {
+    val (result, operands) = line.split(": ")
+        .let { (result, operands) -> result.toLong() to operands.split(' ').map { it.toLong() } }
+    if (check(operands, result)) total += result
+}
+
+println(total)
